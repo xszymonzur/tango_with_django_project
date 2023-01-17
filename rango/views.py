0f571096ae83b2410d6dfd1ea7028f64b9ce35e5
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-01-17 15:42:01 +0000

    Add anchors to responses

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,8 @@ from django.http import HttpRequest
 
 
 def index(request: HttpRequest):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+
+def about(request: HttpRequest):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
