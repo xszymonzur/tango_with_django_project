c76f3d5d593e47e36b63e56aed8109d4c4aa87fe
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-01-21 17:42:17 +0000

    Create data-driven pages, useslugs

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
+from rango.models import Category, Page
 
 
 def index(request: HttpRequest):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
 
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def show_category(request: HttpRequest, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['category'] = category
+        context_dict['pages'] = pages
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
 
 
 def about(request: HttpRequest):
