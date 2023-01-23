c2548cab6e1ef21d2636cb25ad8fa0194f4d0c7f
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-01-23 20:53:04 +0000

    Add add_category from

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.urls import reverse
+
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request: HttpRequest):
@@ -29,6 +32,21 @@ def show_category(request: HttpRequest, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request: HttpRequest):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+    if form.is_valid():
+        form.save(commit=True)
+
+        return redirect('/rango/')
+    else:
+        print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
 
 def about(request: HttpRequest):
