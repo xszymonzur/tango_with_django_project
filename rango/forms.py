3c45c13a09e26cb368a04b73a1df03d0516b420e
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-01-23 21:02:19 +0000

    Add add_page form

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -14,9 +14,9 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0, required=False)
 
     class Meta:
         model = Page
