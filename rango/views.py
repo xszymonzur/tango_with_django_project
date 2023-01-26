c0f050b9ed622246492948b3b7a145ff8934bbf0
Szymon Zurawski <szymon.zurawski03@gmail.com>
2023-01-26 23:35:45 +0000

    Create and use template tags

--- rango/views.py
+++ rango/views.py
@@ -54,7 +54,7 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
-    # You cannot add a page to a Category that does not exist...
+
     if category is None:
         return redirect('/rango/')
 
